9e1e53b274621b0b3ca1efe6955292f1bc73d9a3
Igor Fagundes [ifaresi] <48635268+igorifaresi@users.noreply.github.com>
2020-11-10 15:40:41 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 wget https://downloads.rclone.org/v1.53.2/rclone-v1.53.2-linux-amd64.zip
-git clone https://github.com/igorifaresi/rclone-backup-tool
+wget https://raw.githubusercontent.com/igorifaresi/rclone-backup-tool/main/rclone-backup-tool
 
 mv rclone-v1.53.2-linux-amd64/rclone-v1.53.2-linux-amd64/rclone /bin/rclone
-mv rclone-backup-tool/rclone-backup-tool /bin/rclone-backup-tool
+mv rclone-backup-tool /bin/rclone-backup-tool
 
 chmod +x /bin/rclone
 chmod +x /bin/rclone-backup-tool
